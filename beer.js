a12099a2ee1c5e7fb6ff91aebe9c12f2d3b17595
SmoczyBaron <73004051+SmoczyBaron@users.noreply.github.com>
2020-12-19 12:29:26 -0600

    background

---
+++ beer.js
@@ -0,0 +1,21 @@
+
+
+ // var for button
+ var searchBtn = document.querySelector(".search-input")
+ var button = document.querySelector(".button")
+ 
+ 
+ searchBtn.addEventListener("keypress", submit)
+ function submit(e){ 
+     if (e.keyCode == 13) {
+     console.log(searchBtn.value)
+     // beerResults{searchBtn.value}
+     }
+ }
+ 
+ 
+ 
+ button.addEventListener("click", search)
+ function search(){
+     
+ }
