94dcc9491098b219a287adf91619bbf2de8142b5
kihBootCamp <kimberleyheuer@hotmail.com>
2021-01-02 12:47:11 -0600

    sticky

--- script.js
+++ script.js
@@ -27,12 +27,8 @@ $(document).ready(function () {
     //var spoonacular_api = "https://api.spoonacular.com/food/menuItems/search?apiKey=873062e3e17f4030a94a2f81ccdb4281&query=chicken%20wings"
 
     var spoonacular_api = "https://api.spoonacular.com/food/menuItems/search?apiKey=873062e3e17f4030a94a2f81ccdb4281";
-<<<<<<< HEAD
-    // apiKey= "0eebf06b3ea84f23a5db250879af45af"
-=======
     //apiKey=0eebf06b3ea84f23a5db250879af45af"
     //apiKey=873062e3e17f4030a94a2f81ccdb4281"
->>>>>>> ac06dbb4d5f053a64ea9ec688869f12813c558e4
 
     var newli;
 
@@ -128,23 +124,11 @@ $(document).ready(function () {
                     // $("#food-pairing" + i).append("<li id=" + fid + "><a>" + beerdata[i].food_pairing[ii] + "</a></li>").on("click", "#" + fid, function (e) {                        
                     //     e.preventDefault();
                         
-<<<<<<< HEAD
                         var food_item = $(this).text();
                         get_spoonacular(food_item, $(e.target).parent().parent().attr("id")); 
                         console.log(this)                   
                     });
                     // $("#food-pairing" + i).append("<li id=" + fid + "><a>" + beerdata[i].food_pairing[ii] + "</a></li>").on("click", "#" + fid, liHandle);
-=======
-                    //     var food_item = $(this).text();
-                    //     get_spoonacular(food_item, $(e.target).parent().parent().attr("id"));                    
-                    // });
-                    $("#food-pairing" + i).append("<li id=" + fid + "><a>" + beerdata[i].food_pairing[ii] + "</a></li>");
-                    //.on("click", "#" + fid, liHandle);
-                    newli =  $("#food-pairing" + i);
-                    //$("#food-pairing" + i).on("click", "#" + fid, liHandle);
-                    newli.on("click", "#" + fid, liHandle);
-                    
->>>>>>> ac06dbb4d5f053a64ea9ec688869f12813c558e4
                 }
             }
         })
